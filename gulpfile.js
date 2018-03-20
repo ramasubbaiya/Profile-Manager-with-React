646219392a21b825646bdea6af686691afda17f9
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-03-19 22:30:32 -0400

    Display table using css

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ const browserSync = require('browser-sync').create();
 
 // Compile Sass
 gulp.task('sass', function() {
-    return gulp.src('src/scss/*.scss')
+    return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream())
 });
 
-// Watch and Serve
+// Watch Sass and Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: './src'
     });
 
-    gulp.watch(['/src/sass/*.scss'], ['sass']);
-    gulp.watch(['/src/js/*.js'], ['js']);
-    gulp.watch(['/src/*.html'], ['html']).on('change', browserSync.reload);
+    gulp.watch(['src/sass/*.scss'], ['sass']);
+    //gulp.watch(['/src/js/*.js'], ['js']);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 // TODO : Minify or Uglify

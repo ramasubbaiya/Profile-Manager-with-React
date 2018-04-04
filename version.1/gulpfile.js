bad1ec5a4a88b69f58b361a87242722a926f3258
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2018-04-03 21:56:39 -0400

    browser-sync reloading and section center

--- version.1/gulpfile.js
+++ version.1/gulpfile.js
@@ -2,22 +2,21 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
-// Compile Sass
+// Compile Sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['src/scss/*.scss'])
+    return gulp.src('src/scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.stream())
 });
 
-// Watch Sass and Serve
+// Watch Sass, html and Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: './src'
     });
 
-    gulp.watch(['src/sass/*.scss'], ['sass']);
-    //gulp.watch(['/src/js/*.js'], ['js']);
+    gulp.watch(['src/scss/*.scss'], ['sass']).on('change', browserSync.reload);
     gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
